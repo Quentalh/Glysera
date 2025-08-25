0774913131208141c70a98fbef1cbc3090cc258c
Heitor Quental <heitorquental321@gmail.com>
2025-08-25 14:04:27 -0300

    slide menu

--- src/components/TelaConsulta/Consulta.jsx
+++ src/components/TelaConsulta/Consulta.jsx
@@ -1,37 +1,41 @@
 import './Consulta.css'
-import { Routes, Route, Link } from 'react-router-dom';
 import { useState } from 'react';
+import LogoNav from '../../assets/LogoNav.png'
 function Consulta() {
     const [nome ,setNome] = useState('');
     const [nmr,setNmr] = useState('');
     const[nascimento,setNascimento] = useState('');
+    const [isMenuOpen, setIsMenuOpen] = useState(false); // New state for the menu
+    const toggleMenu = () => {setIsMenuOpen(!isMenuOpen);};
+
     return(
         <div className="Consulta">
             <nav>
-                <ul>
-                    <li>INICIO</li>
-                    <li>AUTENTICACAO</li>
-                    <li>CADASTROS</li>
-                    <li>GERENCIAMENTO</li>
-                    <li>CONSULTAS</li>
-                </ul>
+                <div className="menu-botao">
+                    <button onClick={toggleMenu} aria-label="Toggle menu" className="menu-btn">
+                        <div className={`menu-line ${isMenuOpen ? 'rotate-45 translate-y-2' : ''}`}></div>
+                        <div className={`menu-line ${isMenuOpen ? 'opacity-0' : ''}`}></div>
+                        <div className={`menu-line ${isMenuOpen ? '-rotate-45 -translate-y-2' : ''}`}></div>
+                    </button>
+                    <img src={LogoNav} alt="Logo da barra" className="LogoNav"/>
+                </div>
             </nav>
-            <h1>INFORMAÇÕES DO PACIENTE</h1>
-            <h2>CONSULTAR CADASTRO</h2>
+            <h1>CONSULTAR CADASTRO DE PACIENTES</h1>
             <div className="nome-p">
-                <label htmlFor="nome-paciente">Nome do Paciente:</label>
-                <input type="text" id="nome-paciente" value={nome} onChange={(e) => setNome(e.target.value)}  />
+                <label htmlFor="nome-paciente">Nome do Paciente</label>
+                <input type="text" placeholder="Digite o nome do paciente"id="nome-paciente" value={nome} onChange={(e) => setNome(e.target.value)}  />
             </div>
             <div className="numero-sus">
-                <label htmlFor="numero-do-sus">Número do SUS:</label>
-                <input type="text" id="numero-do-sus" value={nmr} onChange={(e) => setNmr(e.target.value)} />
+                <label htmlFor="numero-do-sus">Número do SUS</label>
+                <input type="text" placeholder="Digite o número do SUS do paciente"id="numero-do-sus" value={nmr} onChange={(e) => setNmr(e.target.value)} />
             </div>
             <div className="data-nascimento">
-                <label htmlFor="data-nascimento">Data de Nascimento:</label>
+                <label htmlFor="data-nascimento">Data de Nascimento</label>
                 <input type="date" id="data-nascimento" value={nascimento} onChange={(e) => setNascimento(e.target.value)} />
             </div>
-            <div className="botao-pesquisa">
+            <div className="botoes">
                 <button>Pesquisar</button>
+                <button>Detalhes</button>
             </div>
             <div className="box">
                 <ul>
@@ -41,6 +45,21 @@ function Consulta() {
                 </ul>
 
             </div>
+            <div
+                className={`overlay ${isMenuOpen ? 'visible' : 'hidden'}`}
+                onClick={toggleMenu}
+            ></div>
+
+            {/* Empty Slide menu */}
+            <nav className={`slide-menu ${isMenuOpen ? 'open' : ''}`}>
+                {/* Add menu items here */}
+                <ul className="slide-menu-list">
+                    <li><button>INICIO</button></li>
+                    <li><button>CADASTROS</button></li>
+                    <li><button>GERENCIAMENTO</button></li>
+                    <li><button>CONSULTAS</button></li>
+                </ul>
+            </nav>
         </div>
     )
 }
--- src/components/TelaLogin/LoginForm.jsx
+++ src/components/TelaLogin/LoginForm.jsx
@@ -8,14 +8,16 @@ import { Link } from 'react-router-dom';
 function LoginForm() {
     return (
         <div id="root">
-            <img src={Logo} alt="Logo Glysera" className={styles.logo} />
-            <h2 className={styles.titulo}>Painel de Acesso</h2>
-            <CPFField />
-            <PasswordField />
-            {/* Wrap the Button with Link */}
-            <Link to="/consulta">
+            <div className={styles.Login}>
+                <img src={Logo} alt="Logo Glysera" className={styles.logo} />
+                <h2 className={styles.titulo}>Painel de Acesso</h2>
+                <CPFField />
+                <PasswordField />
+                {/* Wrap the Button with Link */}
+                <Link to="/consulta">
                 <Button />
-            </Link>
+                </Link>
+            </div>
         </div>
     );
 }
